037887466fc2bd00c429330107b6f7ae3c873b61
Surendra Yalamarthi <surendra.yalamarthi@gmail.com>
2023-01-10 14:39:02 +0530

    implemented wepack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+       directory: path.resolve(__dirname, 'dist')
+    },
+    open: true,
+    client: {
+      logging: 'none'
+    },
+    port: 9000
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  }
+}
