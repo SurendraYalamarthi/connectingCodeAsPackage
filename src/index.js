3847334a930f1e645eda6892417bed0a84eef557
Surendra Yalamarthi <surendra.yalamarthi@gmail.com>
2023-01-10 18:51:01 +0530

    implemented wepack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-export { default } from './components/App'; 
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+import App from './components/App'
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(<App />);
